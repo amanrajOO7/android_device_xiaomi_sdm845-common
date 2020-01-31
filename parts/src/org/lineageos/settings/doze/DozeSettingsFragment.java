b21205997218a1b27f0c6f3fcf8cdbe6e37a99fd
Nauval Rizky <enuma.alrizky@gmail.com>
2020-01-31 11:57:48 +0900

    sdm845-common: parts: Introduce raise to wake gesture
    * Highly inspired by iOS raise to wake
    
    Change-Id: Ic7b3c1c9228e012a319c8d8423718eb4687d65e9
    Signed-off-by: Nauval Rizky <enuma.alrizky@gmail.com>

--- parts/src/org/lineageos/settings/doze/DozeSettingsFragment.java
+++ parts/src/org/lineageos/settings/doze/DozeSettingsFragment.java
@@ -47,6 +47,7 @@ public class DozeSettingsFragment extends PreferenceFragment implements
     private SwitchPreferenceCompat mPickUpPreference;
     private SwitchPreferenceCompat mHandwavePreference;
     private SwitchPreferenceCompat mPocketPreference;
+    private SwitchPreferenceCompat mRaiseToWakePreference;
 
     private Handler mHandler = new Handler();
 
@@ -88,6 +89,10 @@ public class DozeSettingsFragment extends PreferenceFragment implements
         mPocketPreference.setEnabled(dozeEnabled);
         mPocketPreference.setOnPreferenceChangeListener(this);
 
+        mRaiseToWakePreference = (SwitchPreferenceCompat) findPreference(DozeUtils.GESTURE_RAISE_TO_WAKE_KEY);
+        mRaiseToWakePreference.setEnabled(dozeEnabled);
+        mRaiseToWakePreference.setOnPreferenceChangeListener(this);
+
         // Hide proximity sensor related features if the device doesn't support them
         if (!DozeUtils.getProxCheckBeforePulse(getActivity())) {
             getPreferenceScreen().removePreference(proximitySensorCategory);
@@ -99,6 +104,8 @@ public class DozeSettingsFragment extends PreferenceFragment implements
         } else {
             pickupSensorCategory.setDependency(DozeUtils.ALWAYS_ON_DISPLAY);
             proximitySensorCategory.setDependency(DozeUtils.ALWAYS_ON_DISPLAY);
+            mPickUpPreference.setDependency(DozeUtils.GESTURE_RAISE_TO_WAKE_KEY);
+            mPocketPreference.setDependency(DozeUtils.GESTURE_RAISE_TO_WAKE_KEY);
         }
     }
 
@@ -127,6 +134,7 @@ public class DozeSettingsFragment extends PreferenceFragment implements
         mAlwaysOnDisplayPreference.setEnabled(isChecked);
 
         mPickUpPreference.setEnabled(isChecked);
+        mRaiseToWakePreference.setEnabled(isChecked);
         mHandwavePreference.setEnabled(isChecked);
         mPocketPreference.setEnabled(isChecked);
     }
